c4868907fcd264aac3b850990cb1e90ec961f603
Ivan Villanueva <ivandiscobolo@gmail.com>
2022-06-14 12:11:28 +0200

    Checkpoint 2

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import csv
 def read_data():
     dictionary = {}
     with open('results.json', 'r') as file:
-        lista_lineas = file.readlines()
-        print("Lista de líneas leídas: ", lista_lineas[1])
-        #dictionary.update(lista_lineas[1]) 
+        lista_lineas = file.read()
+        dictionary = lista_lineas
 
     return dictionary
 
+#def get_name_description(key, dictionary):
+    #print(dictionary["name"])
+    #print(dictionary["description"])
+
 if __name__ == "__main__":
     dic = read_data()
-    print(dic)
+    print(type(dic))
+    #get_name_description(1020, dic)
+   
